9855afac1c19ad1c86b46e11c58fa6fa94ded2ea
josetor101 <josetordecilla800@gmail.com>
2023-11-01 10:26:47 -0500

    Clave de GCP modificada

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '2hl5684eyz&6jd+3-rx=vg8enjb+d=f06p^lt^z%%nel1qds_q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['34.171.145.210']
+ALLOWED_HOSTS = ['35.192.43.197']
 
 
 # Application definition
